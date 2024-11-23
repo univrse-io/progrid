ff40ac98e9d8e6493af53e875666788b9778f53e
fa-fifi <fafifi1997@gmail.com>
2024-11-24 01:44:12 +0800

    🔧 Setup firebase for iOS and Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:progrid/models/tower_provider.dart';
-
 import 'package:progrid/models/user_provider.dart';
 import 'package:progrid/services/auth_wrapper.dart';
 import 'package:progrid/services/themes.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
