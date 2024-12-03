8b7d37d35a7ef8f33e7f8c37a84dc569ded610f4
Marcel Putra <marcelino.ares.putra@gmail.com>
2024-12-03 12:00:07 +0800

    basic towers stream models setup

--- lib/models/providers/tower_provider.dart
+++ lib/models/providers/tower_provider.dart
@@ -8,24 +8,16 @@ import 'package:progrid/models/tower.dart';
 class TowersProvider extends ChangeNotifier {
   List<Tower> towers = [];
 
-  // run this to refresh towers list
-  Future<void> fetchTowers() async {
-    try {
-      towers = []; // reset list
-      final snapshot = await FirebaseFirestore.instance.collection('towers').get();
-
-      // fetch each tower
-      for (final doc in snapshot.docs) {
-        final Tower tower = await Tower.fromFirestore(doc);
-        towers.add(tower);
-      }
-      notifyListeners();
-    } catch (e) {
-      print("Error Fetching Towers: $e");
-    }
+  // initialize towers stream
+  Stream<List<Tower>> getTowersStream() {
+    return FirebaseFirestore.instance.collection('towers').snapshots().map(
+      (snapshot) {
+        return snapshot.docs.map((doc) => Tower.fromFirestore(doc)).toList();
+      },
+    );
   }
 
-  // update issue status in a tower
+  // update a tower's issue's status
   void updateIssueStatus(String towerId, String issueId, String status) {
     try {
       final tower = towers.firstWhere((tower) => tower.id == towerId, orElse: () => throw Exception("Tower not found"));
