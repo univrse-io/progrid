67a1e855dc64737c6c2dbdf92cfaa9de033a3915
fa-fifi <fafifi1997@gmail.com>
2024-12-04 06:50:47 +0800

    🚧 Load towers into the dashboard

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:progrid/models/providers/tower_provider.dart';
+import 'package:provider/provider.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -10,8 +12,17 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
+        appBar: AppBar(title: Text('SAPURA GIRN - PROJECT MONITORING REPORT')),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ...context
+                  .watch<TowersProvider>()
+                  .towers
+                  .map((tower) => Text(tower.toString())),
+            ],
+          ),
         ),
       );
 }
