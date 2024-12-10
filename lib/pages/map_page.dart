857abc35b0730cb8601a0585c89527109e2be891
Marcel Putra <marcelino.ares.putra@gmail.com>
2024-12-10 16:22:54 +0800

    implement region jumping

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -28,34 +28,7 @@ class _MapPageState extends State<MapPage> {
   // TODO: move this to firebase server, to allow switching incase of tile server crashes
   final String _tileLayerUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-  // configure map tiles here
-  // Widget _mapThemeTileBuilder(
-  //   BuildContext context,
-  //   Widget tileWidget,
-  //   TileImage tile,
-  // ) {
-  //   // black and white light theme
-  //   return ColorFiltered(
-  //     colorFilter: const ColorFilter.mode(
-  //       Colors.white,
-  //       BlendMode.saturation,
-  //     ),
-  //     child: tileWidget,
-  //   );
-
-  //   // dark theme
-  //   // return ColorFiltered(
-  //   //   colorFilter: const ColorFilter.matrix(<double>[
-  //   //     -0.2126, -0.7152, -0.0722, 0, 255, // red channel
-  //   //     -0.2126, -0.7152, -0.0722, 0, 255, // green channel
-  //   //     -0.2126, -0.7152, -0.0722, 0, 255, // blue channel
-  //   //     0, 0, 0, 1, 0, // alpha channel
-  //   //   ]),
-  //   //   child: tileWidget,
-  //   // );
-  // }
-
-  // determing region color here
+  // determine region color here
   Color _getRegionColor(String region) {
     switch (region.toLowerCase()) {
       case 'southern':
@@ -77,12 +50,43 @@ class _MapPageState extends State<MapPage> {
     }
   }
 
+  // detemine region average positions here
+  final Map<String, LatLng> _regionPositions = {
+    'southern': LatLng(2.0953, 103.0404),
+    'northern': LatLng(5.1152, 100.4532),
+    'eastern': LatLng(4.3120, 102.4632),
+    'central': LatLng(3.0147, 101.3747),
+    'western': LatLng(4.1846, 100.6604),
+    'sabah': LatLng(5.9804, 116.0735),
+    'sarawak': LatLng(1.5548, 110.3592),
+  };
+
+  // determine region zoom levels here
+  final Map<String, double> _regionZoomLevels = {
+    'southern': 12.0,
+    'northern': 13.0,
+    'eastern': 12.5,
+    'central': 12.0,
+    'western': 13.0,
+    'sabah': 11.0,
+    'sarawak': 11.5,
+  };
+
   @override
   Widget build(BuildContext context) {
     final towersProvider = Provider.of<TowersProvider>(context);
 
-    final int surveyedCount = towersProvider.towers.where((tower) => tower.status == 'surveyed').length;
-    final int towerCount = towersProvider.towers.length;
+    // final int surveyedCount = towersProvider.towers.where((tower) => tower.status == 'surveyed').length;
+    // final int towerCount = towersProvider.towers.length;
+
+    // function to go to a specific region
+    void _zoomToRegion(String region) {
+      final LatLng? position = _regionPositions[region];
+      final double? zoomLevel = _regionZoomLevels[region];
+      if (position != null) {
+        _mapController.move(position, zoomLevel!);
+      }
+    }
 
     return Scaffold(
       body: Stack(
@@ -203,28 +207,74 @@ class _MapPageState extends State<MapPage> {
             ],
           ),
 
-          // top row
+          // top left
           Positioned(
-            top: 30,
+            top: 25,
+            left: 14,
+            child: FloatingActionButton(
+              onPressed: () async {
+                final String? selectedRegion = await showMenu(
+                  elevation: 0,
+                  color: Colors.black.withOpacity(0),
+                  context: context,
+                  position: RelativeRect.fromLTRB(0, 70, 0, 0),
+                  items: _regionPositions.keys.map((region) {
+                    return PopupMenuItem(
+                      height: 35,
+                      value: region,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          '${region[0].toUpperCase()}${region.substring(1)}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                );
+
+                if (selectedRegion != null) {
+                  _zoomToRegion(selectedRegion);
+                }
+              },
+              backgroundColor: Colors.black.withOpacity(0.6),
+              child: const Icon(
+                Icons.map,
+                size: 32,
+              ),
+              mini: true,
+            ),
+          ),
+
+          // top right row
+          Positioned(
+            top: 25,
             right: 14,
             child: Row(
               children: [
-                // tower counter
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.6),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Text(
-                    'Surveyed: $surveyedCount / Total: $towerCount',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+                // // debug tower counter
+                // Container(
+                //   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                //   decoration: BoxDecoration(
+                //     color: Colors.black.withOpacity(0.6),
+                //     borderRadius: BorderRadius.circular(8),
+                //   ),
+                //   child: Text(
+                //     'Surveyed: $surveyedCount / Total: $towerCount',
+                //     style: const TextStyle(
+                //       color: Colors.white,
+                //       fontSize: 16,
+                //       fontWeight: FontWeight.bold,
+                //     ),
+                //   ),
+                // ),
                 const SizedBox(width: 5),
 
                 // search button
