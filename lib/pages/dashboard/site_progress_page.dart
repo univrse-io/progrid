33875009dbb733a4ea619f1ede4075c789096f0c
fa-fifi <fafifi1997@gmail.com>
2025-01-03 18:38:00 +0800

    🚧 Create checkboxes for filter

--- lib/pages/dashboard/site_progress_page.dart
+++ lib/pages/dashboard/site_progress_page.dart
@@ -4,15 +4,18 @@ import 'package:progrid/models/tower.dart';
 import 'package:progrid/services/firestore.dart';
 import 'package:provider/provider.dart';
 
-class AsBuiltDrawingPage extends StatefulWidget {
-  const AsBuiltDrawingPage({super.key});
+class SiteProgressPage extends StatefulWidget {
+  const SiteProgressPage({super.key});
 
   @override
-  State<AsBuiltDrawingPage> createState() => _AsBuiltDrawingPageState();
+  State<SiteProgressPage> createState() => _SiteProgressPageState();
 }
 
-class _AsBuiltDrawingPageState extends State<AsBuiltDrawingPage>
+class _SiteProgressPageState extends State<SiteProgressPage>
     with AutomaticKeepAliveClientMixin {
+  final surveyStatusFilter = <String>[];
+  final drawingStatusFilter = <DrawingStatus>[];
+
   @override
   bool get wantKeepAlive => true;
 
@@ -21,47 +24,117 @@ class _AsBuiltDrawingPageState extends State<AsBuiltDrawingPage>
     super.build(context);
     final towers = Provider.of<List<Tower>>(context);
 
-    return SingleChildScrollView(
-      child: Column(
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ...towers.map((tower) => Visibility(
-                visible: tower.surveyStatus == 'surveyed',
-                child: Card(
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: ListTile(
-                    trailing: ToggleButtons(
-                        borderRadius: BorderRadius.circular(20),
-                        onPressed: (index) =>
-                            FirestoreService.updateTower(tower.id, data: {
-                              'drawingStatus': DrawingStatus.values
-                                  .where((status) =>
-                                      status != DrawingStatus.incomplete)
-                                  .toList()[index]
-                                  .name
-                            }),
-                        fillColor: Colors.green.shade100,
-                        selectedBorderColor: Colors.green.shade700,
-                        children: [
-                          ...DrawingStatus.values
-                              .where((status) =>
-                                  status != DrawingStatus.incomplete)
-                              .map((status) => Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10),
-                                    child: Text(status.toString()),
-                                  )),
-                        ],
-                        isSelected: [
-                          ...DrawingStatus.values
-                              .where((status) =>
-                                  status != DrawingStatus.incomplete)
-                              .map((status) => tower.drawingStatus == status)
-                        ]),
-                    title: Text(tower.name),
-                    subtitle: Text(tower.id),
-                  ),
+          Expanded(
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Filters',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 15),
+                    Text('On-Site Audit'),
+                    CheckboxListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text('In Progress'),
+                        value: surveyStatusFilter.contains('in-progress'),
+                        onChanged: (value) => setState(() => value!
+                            ? surveyStatusFilter.add('in-progress')
+                            : surveyStatusFilter.remove('in-progress'))),
+                    CheckboxListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text('Completed'),
+                        value: surveyStatusFilter.contains('surveyed'),
+                        onChanged: (value) => setState(() => value!
+                            ? surveyStatusFilter.add('surveyed')
+                            : surveyStatusFilter.remove('surveyed'))),
+                    CheckboxListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text('Balance'),
+                        value: surveyStatusFilter.contains('unsurveyed'),
+                        onChanged: (value) => setState(() => value!
+                            ? surveyStatusFilter.add('unsurveyed')
+                            : surveyStatusFilter.remove('unsurveyed'))),
+                    SizedBox(height: 15),
+                    Text('As-Built Drawing'),
+                    ...DrawingStatus.values.map((status) => CheckboxListTile(
+                        dense: true,
+                        contentPadding: EdgeInsets.zero,
+                        controlAffinity: ListTileControlAffinity.leading,
+                        title: Text(status.toString()),
+                        value: drawingStatusFilter.contains(status),
+                        onChanged: (value) => setState(() => value!
+                            ? drawingStatusFilter.add(status)
+                            : drawingStatusFilter.remove(status))))
+                  ],
                 ),
-              ))
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ...towers.map((tower) => Visibility(
+                        visible: tower.surveyStatus == 'surveyed',
+                        child: Card(
+                          margin: EdgeInsets.fromLTRB(5, 0, 0, 5),
+                          child: ListTile(
+                            trailing: ToggleButtons(
+                                borderRadius: BorderRadius.circular(20),
+                                onPressed: (index) =>
+                                    FirestoreService.updateTower(tower.id,
+                                        data: {
+                                          'drawingStatus': DrawingStatus.values
+                                              .where((status) =>
+                                                  status !=
+                                                  DrawingStatus.incomplete)
+                                              .toList()[index]
+                                              .name
+                                        }),
+                                fillColor: Colors.green.shade100,
+                                selectedBorderColor: Colors.green.shade700,
+                                children: [
+                                  ...DrawingStatus.values
+                                      .where((status) =>
+                                          status != DrawingStatus.incomplete)
+                                      .map((status) => Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 10),
+                                            child: Text(status.toString()),
+                                          )),
+                                ],
+                                isSelected: [
+                                  ...DrawingStatus.values
+                                      .where((status) =>
+                                          status != DrawingStatus.incomplete)
+                                      .map((status) =>
+                                          tower.drawingStatus == status)
+                                ]),
+                            title: Text(tower.name),
+                            subtitle: Text(tower.id),
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
