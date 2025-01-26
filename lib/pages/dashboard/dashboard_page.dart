93f9b49a38d97300455705d942f941dd4f409a39
fa-fifi <fafifi1997@gmail.com>
2025-01-27 02:59:00 +0800

    💄 Change drawer to tabs

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -24,69 +24,71 @@ class _DashboardPageState extends State<DashboardPage> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Image.asset('assets/images/sapura.png', height: 40),
-            SizedBox(width: 20),
-            Image.asset('assets/images/binasat.png', height: 40),
-            SizedBox(width: 20),
-            Image.asset('assets/images/uos.png', height: 40),
-            SizedBox(width: 20),
-            Text('PROJECT MONITORING REPORT SYSTEM',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-          ],
-        ),
-        actions: [
-          Card.filled(
-            color: Colors.black12,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.schedule),
-                  SizedBox(width: 5),
-                  StreamBuilder<DateTime>(
-                      stream: currentUpdatedTime(),
-                      builder: (context, snapshot) => Text(
-                          DateFormat('d/M/y h:m a')
-                              .format(snapshot.data ?? DateTime.now()))),
-                  SizedBox(width: 5),
-                ],
-              ),
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            backgroundColor: Colors.grey.shade200,
+            appBar: AppBar(
+              title: Row(mainAxisSize: MainAxisSize.min, children: [
+                Image.asset('assets/images/sapura.png', height: 35),
+                SizedBox(width: 20),
+                Image.asset('assets/images/binasat.png', height: 35),
+                SizedBox(width: 20),
+                Image.asset('assets/images/uos.png', height: 45),
+                SizedBox(width: 20),
+                Text('PROJECT MONITORING REPORT SYSTEM',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Expanded(
+                  child: TabBar(
+                      isScrollable: true,
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      indicatorColor: Colors.blueAccent.shade700,
+                      labelStyle: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(fontWeight: FontWeight.bold),
+                      unselectedLabelStyle: TextStyle(color: Colors.black45),
+                      indicatorWeight: 3,
+                      dividerHeight: 0,
+                      tabs: [
+                        Tab(icon: Text('Home')),
+                        Tab(icon: Text('Site Progress')),
+                      ]),
+                ),
+              ]),
+              actions: [
+                Card.filled(
+                  color: Colors.black12,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(width: 5),
+                        StreamBuilder<DateTime>(
+                            stream: currentUpdatedTime(),
+                            builder: (context, snapshot) => Text(
+                                DateFormat('d/M/y h:m a')
+                                    .format(snapshot.data ?? DateTime.now()))),
+                        SizedBox(width: 5),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 5),
+                IconButton(
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ProfilePage())),
+                    icon: Icon(Icons.person)),
+                SizedBox(width: 5),
+              ],
             ),
-          ),
-          SizedBox(width: 5),
-          IconButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage())),
-              icon: Icon(Icons.person)),
-          SizedBox(width: 5),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            ListTile(
-              title: const Text('Home'),
-              onTap: () => pageController.jumpToPage(0),
-            ),
-            ListTile(
-              title: const Text('Site Progress'),
-              onTap: () => pageController.jumpToPage(1),
-            ),
-          ],
-        ),
-      ),
-      body: PageView(
-          controller: pageController,
-          children: [
-            HomePage(),
-            SiteProgressPage(),
-          ],
-          physics: NeverScrollableScrollPhysics()));
+            body: TabBarView(children: [
+              HomePage(),
+              SiteProgressPage(),
+            ])),
+      );
 }
