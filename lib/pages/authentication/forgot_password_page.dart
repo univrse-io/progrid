bd9b7933f55dda66a1b47ff97ef7140c20e8641c
fa-fifi <fafifi1997@gmail.com>
2025-04-15 20:58:32 +0800

    👔 Modify authentication page navigation flow

--- lib/pages/authentication/forgot_password_page.dart
+++ lib/pages/authentication/forgot_password_page.dart
@@ -4,110 +4,90 @@ import 'package:carbon_design_system/carbon_design_system.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ForgotPasswordPage extends StatefulWidget {
-  const ForgotPasswordPage({super.key});
+class ForgotPasswordPage extends StatelessWidget {
+  final TextEditingController emailController;
+  final PageController pageController;
+
+  const ForgotPasswordPage({
+    required this.emailController,
+    required this.pageController,
+    super.key,
+  });
 
   @override
-  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
-}
-
-class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
-  late final carbonToken = Theme.of(context).extension<CarbonToken>();
-  final emailController = TextEditingController();
-
-  // TODO: Restructure forgot password function.
-  Future<void> forgotPassword() async {
-    final email = emailController.text.trim();
-
-    if (email.isEmpty) {
-      unawaited(
-        showDialog(
-          context: context,
-          builder:
-              (_) => const AlertDialog(
-                title: Text('Please Enter an Email Address'),
-              ),
-        ),
-      );
-      return;
-    }
-
-    try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-
-      if (mounted) {
-        Navigator.pop(context);
-        unawaited(
-          showDialog(
-            context: context,
-            builder:
-                (_) => const AlertDialog(
-                  title: Text('Password reset email sent. Check your inbox'),
-                ),
-          ),
-        );
-      }
-    } on FirebaseAuthException catch (e) {
-      if (mounted) {
-        unawaited(
-          showDialog(
-            context: context,
-            builder: (_) => AlertDialog(title: Text(e.message!)),
-          ),
-        );
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      automaticallyImplyLeading: false,
-      title: Image.asset('assets/images/progrid_black.png', height: 35),
-    ),
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Spacing.$7(),
-          Text('Reset password', style: CarbonTextStyle.heading05),
-          const Spacing.$2(),
-          Row(
-            children: [
-              const Text('Now you remember? '),
-              CarbonLink(
-                onPressed: Navigator.of(context).pop,
-                label: 'Return',
-                isInline: true,
-              ),
-            ],
-          ),
-          const Spacing.$7(),
-          const Divider(),
-          const Spacing.$5(),
-          // TODO: Validate email input.
-          CarbonTextInput(controller: emailController, labelText: 'Email'),
-          const Spacing.$5(),
-          FilledButton(onPressed: forgotPassword, child: const Text('Confirm')),
-        ],
-      ),
-    ),
-    bottomNavigationBar: ListTile(
-      tileColor: carbonToken?.backgroundInverse,
-      textColor: carbonToken?.textInverse,
-      title: const Text.rich(
-        TextSpan(
-          text: 'Powered by ',
+  Widget build(BuildContext context) => SingleChildScrollView(
+    padding: const EdgeInsets.all(16),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Spacing.$7(),
+        Text('Reset password', style: CarbonTextStyle.heading05),
+        const Spacing.$2(),
+        Row(
           children: [
-            TextSpan(
-              text: 'UniVRse',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            const Text('Now you remember? '),
+            CarbonLink(
+              onPressed: () => pageController.jumpToPage(0),
+              label: 'Return',
+              isInline: true,
             ),
           ],
         ),
-      ),
+        const Spacing.$7(),
+        const Divider(),
+        const Spacing.$5(),
+        // TODO: Validate email input.
+        CarbonTextInput(controller: emailController, labelText: 'Email'),
+        const Spacing.$5(),
+        FilledButton(
+          // TODO: Restructure forgot password function.
+          onPressed: () async {
+            final email = emailController.text.trim();
+
+            if (email.isEmpty) {
+              unawaited(
+                showDialog(
+                  context: context,
+                  builder:
+                      (_) => const AlertDialog(
+                        title: Text('Please Enter an Email Address'),
+                      ),
+                ),
+              );
+              return;
+            }
+
+            try {
+              await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+
+              if (context.mounted) {
+                unawaited(
+                  showDialog(
+                    context: context,
+                    builder:
+                        (_) => const AlertDialog(
+                          title: Text(
+                            'Password reset email sent. Check your inbox',
+                          ),
+                        ),
+                  ),
+                );
+              }
+            } on FirebaseAuthException catch (e) {
+              if (context.mounted) {
+                unawaited(
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(title: Text(e.message!)),
+                  ),
+                );
+              }
+            }
+          },
+          child: const Text('Confirm'),
+        ),
+      ],
     ),
   );
 }
--- lib/pages/authentication/register_page.dart
+++ lib/pages/authentication/register_page.dart
@@ -7,118 +7,99 @@ import 'package:flutter/material.dart';
 import '../../services/firebase_auth.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final PageController pageController;
+  final GlobalKey<FormState> formKey;
+
+  const RegisterPage({
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController,
+    required this.pageController,
+    required this.formKey,
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    late final carbonToken = Theme.of(context).extension<CarbonToken>();
-    final nameController = TextEditingController();
-    final emailController = TextEditingController();
-    final passwordController = TextEditingController();
-    final formKey = GlobalKey<FormState>();
-
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Image.asset('assets/images/progrid_black.png', height: 35),
-      ),
-      body: Form(
-        key: formKey,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
+  Widget build(BuildContext context) => Form(
+    key: formKey,
+    child: SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Spacing.$7(),
+          Text('Hello there!', style: CarbonTextStyle.heading05),
+          const Spacing.$2(),
+          Row(
             children: [
-              const Spacing.$7(),
-              Text('Hello there!', style: CarbonTextStyle.heading05),
-              const Spacing.$2(),
-              Row(
-                children: [
-                  const Text('Have an account? '),
-                  CarbonLink(
-                    onPressed: Navigator.of(context).pop,
-                    label: 'Login',
-                    isInline: true,
-                  ),
-                ],
-              ),
-              const Spacing.$7(),
-              const Divider(),
-              const Spacing.$5(),
-              CarbonTextInput(
-                controller: nameController,
-                labelText: 'Full Name',
-                maxCharacters: 20,
-              ),
-              const Spacing.$3(),
-              // TODO: Validate email input.
-              CarbonTextInput(controller: emailController, labelText: 'Email'),
-              const Spacing.$3(),
-              CarbonTextInput(
-                controller: passwordController,
-                labelText: 'Password',
-                obscureText: true,
-              ),
-              const Spacing.$3(),
-              CarbonTextInput(
-                labelText: 'Confirm Password',
-                obscureText: true,
-                validator:
-                    (value) =>
-                        passwordController.text != value
-                            ? "Passwords don't match"
-                            : null,
-              ),
-              const Spacing.$5(),
-              FilledButton(
-                onPressed: () {
-                  if (!formKey.currentState!.validate()) return;
-
-                  FirebaseAuthService()
-                      .register(
-                        nameController.text.trim(),
-                        emailController.text.trim(),
-                        passwordController.text.trim(),
-                      )
-                      .onError<FirebaseAuthException>((e, _) {
-                        if (context.mounted) {
-                          unawaited(
-                            showDialog(
-                              context: context,
-                              builder:
-                                  (_) => AlertDialog(
-                                    title: Text(e.message ?? e.code),
-                                  ),
-                            ),
-                          );
-                        }
-                      })
-                      .then((_) {
-                        if (context.mounted) Navigator.pop(context);
-                      });
-                },
-                child: const Text('Register'),
+              const Text('Have an account? '),
+              CarbonLink(
+                onPressed: () => pageController.jumpToPage(0),
+                label: 'Login',
+                isInline: true,
               ),
             ],
           ),
-        ),
-      ),
-      bottomNavigationBar: ListTile(
-        tileColor: carbonToken?.backgroundInverse,
-        textColor: carbonToken?.textInverse,
-        title: const Text.rich(
-          TextSpan(
-            text: 'Powered by ',
-            children: [
-              TextSpan(
-                text: 'UniVRse',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ],
+          const Spacing.$7(),
+          const Divider(),
+          const Spacing.$5(),
+          CarbonTextInput(
+            controller: nameController,
+            labelText: 'Full Name',
+            maxCharacters: 20,
           ),
-        ),
+          const Spacing.$3(),
+          // TODO: Validate email input.
+          CarbonTextInput(controller: emailController, labelText: 'Email'),
+          const Spacing.$3(),
+          CarbonTextInput(
+            controller: passwordController,
+            labelText: 'Password',
+            obscureText: true,
+          ),
+          const Spacing.$3(),
+          CarbonTextInput(
+            labelText: 'Confirm Password',
+            obscureText: true,
+            validator:
+                (value) =>
+                    passwordController.text != value
+                        ? "Passwords don't match"
+                        : null,
+          ),
+          const Spacing.$5(),
+          FilledButton(
+            onPressed: () {
+              if (!formKey.currentState!.validate()) return;
+
+              FirebaseAuthService()
+                  .register(
+                    nameController.text.trim(),
+                    emailController.text.trim(),
+                    passwordController.text.trim(),
+                  )
+                  .onError<FirebaseAuthException>((e, _) {
+                    if (context.mounted) {
+                      unawaited(
+                        showDialog(
+                          context: context,
+                          builder:
+                              (_) =>
+                                  AlertDialog(title: Text(e.message ?? e.code)),
+                        ),
+                      );
+                    }
+                  })
+                  .then((_) => pageController.jumpTo(0));
+            },
+            child: const Text('Register'),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
