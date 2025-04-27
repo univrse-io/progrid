ff4ac016792e68bb913e623654bd163ae866b389
fa-fifi <fafifi1997@gmail.com>
2025-04-27 13:53:42 +0800

    🐛 Change marker cluster layer text color to white

--- lib/widgets/open_street_map.dart
+++ lib/widgets/open_street_map.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:carbon_design_system/carbon_design_system.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +12,8 @@ import '../models/survey_status.dart';
 import '../models/tower.dart';
 import '../pages/tower_page.dart';
 import '../utils/dialog_utils.dart';
-import '../utils/pie_chart_painter.dart';
+
+final _textStyle = CarbonTextStyle.label01.copyWith(color: Colors.white);
 
 class OpenStreetMap extends StatelessWidget {
   final MapController? controller;
@@ -77,13 +80,7 @@ class OpenStreetMap extends StatelessWidget {
                               const Spacing.$2(),
                               Spacing.$3(color: tower.surveyStatus.color),
                               const Spacing.$2(),
-                              Text(
-                                tower.id,
-                                style:
-                                    Theme.of(
-                                      context,
-                                    ).primaryTextTheme.labelSmall,
-                              ),
+                              Text(tower.id, style: _textStyle),
                               const Spacing.$2(),
                             ],
                           ),
@@ -127,4 +124,57 @@ class OpenStreetMap extends StatelessWidget {
       ],
     );
   }
+}
+
+class PieChartPainter extends CustomPainter {
+  final Map<SurveyStatus, int> statusCounts;
+
+  PieChartPainter(this.statusCounts);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final total = statusCounts.values.reduce((a, b) => a + b);
+    final arcPaint =
+        Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 4;
+    final circlePaint = Paint()..color = Colors.black54;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width / 2, size.height / 2);
+    var startAngle = -pi / 2;
+
+    canvas.drawCircle(center, radius, circlePaint);
+
+    for (final entry in statusCounts.entries) {
+      if (entry.value > 0) {
+        final sweepAngle = (entry.value / total) * 2 * pi;
+        arcPaint.color = entry.key.color;
+
+        canvas.drawArc(
+          Rect.fromCircle(center: center, radius: radius),
+          startAngle,
+          sweepAngle,
+          false,
+          arcPaint,
+        );
+        startAngle += sweepAngle;
+      }
+    }
+
+    final textPainter = TextPainter(
+      text: TextSpan(text: total.toString(), style: _textStyle),
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    textPainter.paint(
+      canvas,
+      Offset(
+        center.dx - textPainter.width / 2,
+        center.dy - textPainter.height / 2,
+      ),
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
