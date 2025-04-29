3dc9320b5ff3e727dfc9e23437708c869edc61d5
fa-fifi <fafifi1997@gmail.com>
2025-04-29 12:35:44 +0800

    💄 Change region colors

--- lib/models/region.dart
+++ lib/models/region.dart
@@ -1,14 +1,15 @@
+import 'package:carbon_design_system/carbon_design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 
 enum Region {
-  central(Color(0xFF3F5164), LatLng(3.0147, 101.3747)),
-  eastern(Color(0xFF867C4F), LatLng(4.3120, 102.4632)),
-  northern(Color(0xFF644444), LatLng(5.1152, 100.4532)),
-  sabah(Color(0xFF3E5858), LatLng(5.9804, 116.0735)),
-  sarawak(Color(0xFFA36E5A), LatLng(1.5548, 110.3592)),
-  southern(Color(0xFF52724C), LatLng(2.0953, 103.0404));
+  central(CarbonColor.purple70, LatLng(3.0147, 101.3747)),
+  eastern(CarbonColor.cyan50, LatLng(4.3120, 102.4632)),
+  northern(CarbonColor.teal70, LatLng(5.1152, 100.4532)),
+  sabah(CarbonColor.magenta70, LatLng(5.9804, 116.0735)),
+  sarawak(CarbonColor.red50, LatLng(1.5548, 110.3592)),
+  southern(CarbonColor.red90, LatLng(2.0953, 103.0404));
 
   final Color color;
   final LatLng latlng;
